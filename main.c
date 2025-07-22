79063663d18efe3ec92b9b6a08a19acd19ab490a
omiiGit <kuldeepbuke@proton.me>
2025-07-22 14:29:48 +0530

    added matrix header

--- main.c
+++ main.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
 
+#define SCREEN_HEIGHT 600
+#define SCREEN_WIDTH 400
+
+#define CELL_WIDTH 20
+#define CELL_HEIGHT 20
+#define COLUMNS SCREEN_HEIGHT/CELL_HEIGHT
+#define ROWS SCREEN_WIDTH/CELL_WIDTH
 
 int main()
 {
-    printf("Cellular autometa");
-
-        
-
     return 0;
 }
 
---
+++ matrix.h
@@ -0,0 +1,14 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#include <SDL2/SDL.h>
+
+typedef struct
+{
+    int cell_width;
+    int cell_height;
+    int columns;
+    int rows;
+}Matrix;
+
+#endif 
