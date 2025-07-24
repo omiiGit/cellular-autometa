e4ec1235fd0d86b65840d4248060fd48abc1e536
omiiGit <kuldeepbuke@proton.me>
2025-07-24 13:51:02 +0530

    updated

---
+++ macro.h
@@ -0,0 +1,14 @@
+#define WHITE 0xffffff
+#define BLACK 0x000000
+#define GREY 0x2f2f2f
+
+#define SCREEN_WIDTH 600
+#define SCREEN_HEIGHT 400
+
+#define CELL_WIDTH 10
+#define CELL_HEIGHT 10
+
+#define COLUMNS SCREEN_WIDTH/CELL_WIDTH
+#define ROWS SCREEN_HEIGHT/CELL_HEIGHT
+
+
---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "macro.h"
+
+int main(int argc, char* argv[])
+{
+    printf("%x\n",WHITE);
+
+    return 0;
+}
