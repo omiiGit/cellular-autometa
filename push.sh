59d7b4a38cb5fc39d9bf08852c2414173e3ad1e5
omiiGit <kuldeepbuke@proton.me>
2025-07-24 14:28:24 +0530

    updated

--- push.sh
+++ push.sh
@@ -7,8 +7,11 @@ if ! pgrep -u $USER "ssh-agent" 1> /dev/null 2> /dev/null
 then
     eval "$(ssh-agent -s)"
     ssh-add $KEY_PATH
+    git add .
+    git commit -m updated 
+    git push origin "updated"
 else
     git add .
     git commit -m updated 
-    git push origin "updated on $DATE"
+    git push origin "updated"
 fi
